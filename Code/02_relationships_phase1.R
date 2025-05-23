32ac848c47a0009ab33506482f80eb440b7e1f99
ksirving <katiei@sccwrp.org>
2025-05-22 22:35:20 -0700

    correlations of daily temp

--- Code/02_relationships_phase1.R
+++ Code/02_relationships_phase1.R
@@ -31,7 +31,8 @@ AllTemp2 <- AllTemp %>%
 
 p1 <- ggplot(data = AllTemp2, aes(x=Temp, y = Score, colour = Mets)) +
   # geom_point() +
-  geom_smooth(method = "loess") +
+  # geom_smooth(method = "lm") +
+  stat_smooth(method = "lm", formula = y ~ x + I(x^2), linewidth = 1)+
   facet_wrap(~BioMetric) +
   theme_bw() +
   theme(legend.position = "bottom",
@@ -43,3 +44,22 @@ file.name1 <- paste0(out.dir, "01_basic_relationships_phase1.jpg")
 ggsave(p1, filename=file.name1, dpi=300, height=6, width=6)
 
 
+
+# Correlations of temp ----------------------------------------------------
+
+head(AllTemp2)
+## format 
+cortemp <- AllTemp2 %>%
+  select(masterid, Year, Month, Day, Temp, Mets, Date) %>%
+  distinct() %>% ## some duplications, fixed later
+  group_by(masterid, Year, Month, Day, Mets, Date) %>%
+  filter(row_number()==1) %>% ## take first row for now
+  pivot_wider(names_from = Mets, values_from = Temp) ## make wide
+names(cortemp)
+
+## only temp data
+cortempx <- cortemp[,6:8]
+
+cors <- cor(cortempx, use = "na.or.complete")
+
+write.csv(cors, "Tables/02_correlations_temp.csv")
